ffe2e09b2d95f15f6be793b18a5366a30df897ba
unknown <hericlysdesa@gmail.com>
2025-07-27 23:57:28 -0300

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,14 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico'],
       manifest: {
-        id: '/',
-        name: 'Nome do Seu App',
-        short_name: 'App',
-        description: 'Descrição do seu PWA',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
+        name: 'ToDoList',
+        short_name: 'ToDo',
         start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#00aaff',
         icons: [
           {
             src: 'pwa-192x192.png',
@@ -28,12 +26,6 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
           }
         ]
       }
